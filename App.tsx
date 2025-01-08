2b0e03334b366be41dadd534bac663faab4ba745
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-01-08 23:29:19 +0530

    add home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import { useEffect, useState } from "react";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWelcome(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {showWelcome ? <WelcomeScreen /> : <HomeScreen />}
     </View>
   );
 }
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
